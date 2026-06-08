eb6bf454e7c8d266799aeb92bbb0756161b60c30
Tazik Shahjahan <tshahjah@uwaterloo.ca>
2026-06-08 03:44:27 -0400

    Add MDX and KaTeX support for notes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,17 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import remarkBreaks from 'remark-breaks';
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
+import mdx from '@astrojs/mdx';
 import rehypeLinkFavicons from './src/rehype-link-favicons.mjs';
 
 export default defineConfig({
   site: 'https://tazik.sh',
+  integrations: [mdx()],
   markdown: {
-    remarkPlugins: [remarkBreaks],
-    rehypePlugins: [rehypeLinkFavicons],
+    remarkPlugins: [remarkBreaks, remarkMath],
+    rehypePlugins: [rehypeLinkFavicons, rehypeKatex],
     shikiConfig: {
       themes: { light: 'github-light', dark: 'github-dark' },
       defaultColor: false,
