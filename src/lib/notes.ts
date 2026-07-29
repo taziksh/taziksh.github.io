7ca0de2dd013c9c167c0377284794acdff9741e4
Tazik Shahjahan <tshahjah@uwaterloo.ca>
2026-07-29 11:11:24 -0500

    Use sentence case for blog post titles

--- src/lib/notes.ts
+++ src/lib/notes.ts
@@ -1,3 +1,8 @@
 export function isBookReview(id: string) {
   return id.split('/')[0]?.toLowerCase() === 'book-notes';
 }
+
+export function formatNoteTitle(title: string) {
+  const lowercaseTitle = title.toLocaleLowerCase('en-US');
+  return lowercaseTitle.replace(/\p{L}/u, (letter) => letter.toLocaleUpperCase('en-US'));
+}
